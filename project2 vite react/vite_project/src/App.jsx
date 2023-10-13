65aea93bde7f1f8eded79ae0628c4a051f7b2188
Mazhar Alam <mazharalamov@gmail.com>
2023-10-13 20:57:11 +0530

    Created react app using vite

---
+++ project2 vite react/vite_project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from './chai.jsx'
+
+function App() {
+
+
+  return (
+    <>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/daisyui/3.8.3/full.css" integrity="sha512-O+Fc80nw5NdS/UwWk8ZQIpJbqYV7qQH6s1E1DwAgJmYwW0AE2liPFEgisBFRfzEfnKCPm3m++Yh04eR9JGk6nA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <h1 className='bg-green-400 text-black p4 rounded-xl'>Hello World</h1>
+
+      
+    </>
+  )
+}
+
+export default App
